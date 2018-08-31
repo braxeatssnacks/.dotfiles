072e907d5a95b4b6eb8d9947d144fd977c94114a
Braxton Gunter <braxton.gun@gmail.com>
2018-08-31 10:46:03 -0700

    disable tmux auto start for now and silence update output

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,10 +1,13 @@
 # Run tmux if exists
-if command -v tmux>/dev/null; then
-	[ -z $TMUX ] && exec tmux
-else
-	echo "tmux not installed. Run ./deploy to configure dependencies ..."
-fi
+# if command -v tmux>/dev/null; then
+# 	[ -z $TMUX ] && exec tmux
+# else
+# 	echo "tmux not installed. Run ./deploy to configure dependencies ..."
+# fi
 
-echo "Updating configuration ..."
-(cd ~/.dotfiles && git pull && git submodule update --init --recursive)
+# implicit update of submodules
+cd ~/.dotfiles
+git pull -q
+git submodule update --init --recursive -q
+
 source ~/.dotfiles/zsh/zshrc.sh
