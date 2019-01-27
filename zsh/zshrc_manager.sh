3fa9aa6ca6013c63d86a17aceee3cec16c224de7
Braxton Gunter <braxton.gun@gmail.com>
2019-01-27 10:36:43 -0800

    add reattachment logic

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -2,7 +2,17 @@ export DOTFILES="$HOME/.dotfiles"
 
 # Run tmux if exists
 if command -v tmux>/dev/null; then
-  [ -z $TMUX ] && exec tmux
+  # attempt to reconnect to existing session or create new
+  if test -z "$TMUX"; then
+    session_num=$(
+    tmux list-sessions  |
+      grep -v attached 	|
+      grep -oE '^\d+:' 	|
+      grep -oE '^\d+' 	|
+      head -1
+    )
+    if test $session_num; then exec tmux attach -t $session_num; else exec tmux; fi
+  fi
 else
   echo "tmux not installed. Run ${DOTFILES/#$HOME/~}/deploy.sh to configure dependencies..."
 fi
